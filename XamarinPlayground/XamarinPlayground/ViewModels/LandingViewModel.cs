70669ed7caffa55521a892667d61b3ef33fe1935
Nick Palacio <npalacio2240@gmail.com>
2018-01-16 19:52:37 -0600

    played with UI

--- XamarinPlayground/XamarinPlayground/ViewModels/LandingViewModel.cs
+++ XamarinPlayground/XamarinPlayground/ViewModels/LandingViewModel.cs
@@ -7,12 +7,14 @@ namespace XamarinPlayground.ViewModels
 {
     public class LandingViewModel : ObservableObject
     {
+        public string Title { get; private set; }
         public ICommand GoToSearch { get; private set; }
         public INavigation Navigation { get; private set; }
         public string SearchBtnText { get; private set; }
 
         public LandingViewModel(INavigation navigation)
         {
+            Title = "Welcome to EmergencyResponse";
             GoToSearch = new Command(GoToEmployeeSearch);
             Navigation = navigation;
             SearchBtnText = "Go to Employee Search";
